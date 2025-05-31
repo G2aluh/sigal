4a8add558e22a7c6d2c512c7d47210ff62e3b8be
galuhssaputra <galuhputrana576@gmail.com>
2025-05-31 10:23:44 +0700

    two commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ export const Navbar = () => {
   return (
     <nav
       className={cn(
-        "fixed w-full z-40 backdrop-blur-2xl opacity-80 shadow-2xs transition-all duration-300 py-5",
+        "fixed w-full z-40 transition-all duration-300 py-5",
      
       )}
     >
